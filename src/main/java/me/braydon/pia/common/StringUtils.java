9a05398810a17c962a990e79e2fd8e5966e318b4
Rainnny7 <braydonrainnny@gmail.com>
2024-04-27 01:10:17 -0400

    Fix wrong capitalization

--- src/main/java/me/braydon/pia/common/StringUtils.java
+++ src/main/java/me/braydon/pia/common/StringUtils.java
@@ -19,7 +19,7 @@ public final class StringUtils {
     public static String capitalizeFully(@NonNull String input, char delimiter) {
         StringBuilder builder = new StringBuilder();
         for (String part : input.split(String.valueOf(delimiter))) {
-            builder.append(part.length() <= 3 ? part.toUpperCase() : capitalize(part)).append(delimiter);
+            builder.append(part.length() <= 2 ? part.toUpperCase() : capitalize(part)).append(delimiter);
         }
         return builder.substring(0, builder.length() - 1).replace(delimiter, ' ');
     }
